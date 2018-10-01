038408ded108fb447e3b3096fab3e73852592819
Erik Hughes <erik.hughes@outlook.com>
2018-10-01 17:35:11 +0200

    refactor: moved platform names to better match react-native
    see https://github.com/DefinitelyTyped/DefinitelyTyped/blob/a44effb2097c6a139dddfe13aabcdfaad51d3ed2/types/react-native/index.d.ts#L5632

--- src/apis/Platform.js
+++ src/apis/Platform.js
@@ -1,5 +1,21 @@
 // @flow
 
-export const OS = process.platform
+
+let OS
+switch (process.platform) {
+  case 'win32':
+    OS = 'windows'
+    break
+  case 'darwin':
+    OS = 'macos'
+    break
+  case 'linux':
+    OS = 'linux'
+    break
+  default:
+    OS = 'web'
+    break
+}
+export const OS
 
 export const select = (obj: Object) => (process.platform in obj ? obj[process.platform] : obj.default)
